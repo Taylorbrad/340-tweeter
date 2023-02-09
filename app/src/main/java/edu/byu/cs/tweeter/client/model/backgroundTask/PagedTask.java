c56bd7457c85abd0b1f0d10ea1e70ed3cb6e937c
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2023-02-08 20:24:50 -0700

    finished lecture stuff

---
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/backgroundTask/PagedTask.java
@@ -0,0 +1,75 @@
+package edu.byu.cs.tweeter.client.model.backgroundTask;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.io.Serializable;
+import java.util.List;
+
+import edu.byu.cs.tweeter.model.domain.AuthToken;
+import edu.byu.cs.tweeter.model.domain.Status;
+import edu.byu.cs.tweeter.model.domain.User;
+import edu.byu.cs.tweeter.util.Pair;
+
+public abstract class PagedTask<T> extends AuthenticatedTask {
+
+
+
+    private List<T> items;
+    private Boolean hasMorePages;
+    /**
+     * The user whose feed is being retrieved.
+     * (This can be any user, not just the currently logged-in user.)
+     */
+    private User targetUser;
+    /**
+     * Maximum number of statuses to return (i.e., page size).
+     */
+    private int limit;
+    /**
+     * The last status returned in the previous page of results (can be null).
+     * This allows the new page to begin where the previous page ended.
+     */
+    private T lastItem;
+
+
+
+
+    public static final String ITEMS_KEY = "items";
+
+    public static final String MORE_PAGES_KEY = "more-pages";
+
+    public PagedTask(Handler messageHandler, AuthToken authToken, User targetUser, int limit, T lastItem) {
+        super(messageHandler, authToken);
+        this.targetUser = targetUser;
+        this.limit = limit;
+        this.lastItem = lastItem;
+    }
+
+    public User getTargetUser() {
+        return targetUser;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public T getLastItem() {
+        return lastItem;
+    }
+
+    @Override
+    protected void loadSuccessBundle(Bundle msgBundle) {
+        msgBundle.putSerializable(ITEMS_KEY, (Serializable) items);//(Serializable) statuses);
+        msgBundle.putBoolean(MORE_PAGES_KEY, hasMorePages);
+    }
+
+    @Override
+    protected void processTask() {
+        Pair<List<T>, Boolean> pageOfStatus = getItems();
+        this.items = pageOfStatus.getFirst();
+        this.hasMorePages = pageOfStatus.getSecond();
+    }
+
+    protected abstract Pair<List<T>, Boolean> getItems();
+}
